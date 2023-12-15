621361152a0a4a043bee0575993ca5cf48aa8b0f
NiklasKerkfeld <s6nikerk@uni-bonn.de>
2023-12-15 15:01:04 +0100

    download and preprocessing

---
+++ src/download.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    # TODO: write script to download GloSAT and our dataset in the Datafolder
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/show_annotations.py
@@ -0,0 +1,12 @@
+"""
+script to demostarte targets for model
+"""
+
+
+def plot(folder: str):
+    """
+    shows target for model from given folder
+    :param folder: path to folder of preprocessed
+    :return:
+    """
+    pass
