bc21c2fe95666151fc9afa9810e850e72a1297f4
NiklasKerkfeld <s6nikerk@uni-bonn.de>
2023-12-21 14:25:32 +0100

    added titles to plots

--- src/show_annotations.py
+++ src/show_annotations.py
@@ -1,30 +1,32 @@
 """
 script to demostarte targets for model
 """
+from typing import Optional
+
 from matplotlib import pyplot as plt
 from pathlib import Path
 import os
 
-def pltbox(image, boundingboxes:list):
+
+def pltbox(image, boundingboxes: list, title: Optional[str] = None):
     """
     plots bounding boxes in image
     """
     plt.imshow(image)
     for box in boundingboxes:
-        #with bounding box x being height and y being width (if np.flip in extract_glosat_annotation)
-        #ymin = box[0]
-        #xmin = box[1]  
-        #ymax = box[2]
-        #xmax =box[3]
-
+        # with bounding box x being height and y being width (if np.flip in extract_glosat_annotation)
         ymin = box[1]
         xmin = box[0]
         ymax = box[3]
-        xmax =box[2]
-        xlist = [xmin,xmin,xmax,xmax,xmin]
-        ylist = [ymin,ymax,ymax,ymin,ymin]
-        plt.plot(xlist, ylist, 'b')
+        xmax = box[2]
+        xlist = [xmin, xmin, xmax, xmax, xmin]
+        ylist = [ymin, ymax, ymax, ymin, ymin]
+        plt.plot(xlist, ylist)
+
+    if title is not None:
+        plt.title(title)
     plt.show()
+
 
 def plot(folder: str):
     """
@@ -32,28 +34,48 @@ def plot(folder: str):
     :param folder: path to folder of preprocessed
     :return:
     """
-    celllist= []
+    tablelist = []
+    celllist = []
     rowlist = []
+    collist = []
     imglist = []
     files = os.listdir(folder)
-    #for root, dirs, files in os.walk(folder):
+    # for root, dirs, files in os.walk(folder):
     for filename in sorted(files):
-        if "row" in filename:
-            with open(folder+filename, 'r') as f:
-                rowdata = [tuple([int(l) for l in line.split()]) for line in f ]
-            rowlist.append(rowdata)
-        if "cell" in filename:
-            with open(folder+filename, 'r') as f:
-                celldata = [tuple([int(l) for l in line.split()]) for line in f ]
-            celllist.append(celldata)
         if "table" in filename and filename.endswith('.jpg'):
-            img = plt.imread(folder+filename)
+            img = plt.imread(folder + filename)
             imglist.append(img)
+
+        if "table" in filename and filename.endswith('.txt'):
+            with open(folder + filename, 'r') as f:
+                tablelist = [tuple([int(l) for l in line.split()]) for line in f]
+
+        if "cell" in filename:
+            with open(folder + filename, 'r') as f:
+                celldata = [tuple([int(l) for l in line.split()]) for line in f]
+            celllist.append(celldata)
+
+        if "row" in filename:
+            with open(folder + filename, 'r') as f:
+                rowdata = [tuple([int(l) for l in line.split()]) for line in f]
+            rowlist.append(rowdata)
+
+        if "col" in filename:
+            with open(folder + filename, 'r') as f:
+                coldata = [tuple([int(l) for l in line.split()]) for line in f]
+            collist.append(coldata)
+
+    pltbox(plt.imread(folder + files[0]), tablelist, title='tables')
+
     for idx, img in enumerate(imglist):
-        pltbox(img, celllist[idx])
-        pltbox(img, rowlist[idx])
+        pltbox(img, celllist[idx], title='cells')
+        pltbox(img, rowlist[idx], title='rows')
+        pltbox(img, collist[idx], title='columns')
+
+
 def main():
     pass
 
+
 if __name__ == '__main__':
-    plot(f'{Path(__file__).parent.absolute()}/../data/preprocessed/4/')
+    plot(f'{Path(__file__).parent.absolute()}/../data/preprocessed/113/')
