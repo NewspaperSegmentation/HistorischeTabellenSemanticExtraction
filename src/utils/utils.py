f992e914b3f343f9a4fe379020c0c861e3f09af3
NiklasKerkfeld <s6nikerk@uni-bonn.de>
2023-12-21 14:25:03 +0100

    added titles to plots

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -14,7 +14,8 @@ def convert_coords(string: str):
     return np.array(points)
 
 
-def get_bbox(points: np.ndarray, corners: Union[None, List[int]] = None, tablebbox: Tuple[int, int, int, int] = None) -> Tuple[int, int, int, int]:
+def get_bbox(points: np.ndarray, corners: Union[None, List[int]] = None, tablebbox: Tuple[int, int, int, int] = None) -> \
+Tuple[int, int, int, int]:
     """
     creates a bounding box around all given points
     :param points: np array of shape (N x 2) containing a list of points
@@ -27,13 +28,11 @@ def get_bbox(points: np.ndarray, corners: Union[None, List[int]] = None, tablebb
 
     x_max, x_min = points[:, 0].max(), points[:, 0].min()
     y_max, y_min = points[:, 1].max(), points[:, 1].min()
-    #swap 0 and 1 for tablebox if np.flip in extract_glosat_annotation
+    # swap 0 and 1 for tablebox if np.flip in extract_glosat_annotation
     if tablebbox:
-        x_min-=tablebbox[0]
-        y_min-=tablebbox[1]
-        x_max-=tablebbox[0]
-        y_max-=tablebbox[1]
-
+        x_min -= tablebbox[0]
+        y_min -= tablebbox[1]
+        x_max -= tablebbox[0]
+        y_max -= tablebbox[1]
 
     return x_min, y_min, x_max, y_max
-
