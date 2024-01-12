967db3ce286b8a0edfbb23d4b327c12cfc28bcac
NiklasKerkfeld <s6nikerk@uni-bonn.de>
2024-01-12 16:46:33 +0100

    add tensorboard + clean up

--- src/customdataset.py
+++ src/customdataset.py
@@ -3,19 +3,19 @@ import glob
 import torch
 import os
 
-class dataset(Dataset):
+
+class CustomDataset(Dataset):
     def __init__(self, path, objective) -> None:
         super().__init__()
-        if objective=="tables":
+        if objective == "tables":
             self.data = list(glob.glob(f"{path}/*"))
         else:
             self.data = list(glob.glob(f"{path}/*/*_table_*"))
         self.objective = objective
 
     def __getitem__(self, index) -> (torch.Tensor, torch.Tensor):
-        #return super().__getitem__(index)
         return torch.randn(3, 1600, 1000), torch.rand((11, 4))
-        if self.objective=="tables":
+        if self.objective == "tables":
             imgnum = self.data[index].split(os.sep)[-1]
             img = torch.load(f"{self.data[index]}/{imgnum}.jpg")
             target = torch.load(f"{self.data[index]}/{imgnum}_tables.txt")
--- src/trainer.py
+++ src/trainer.py
@@ -1,29 +1,49 @@
+import os
+from pathlib import Path
+from tqdm import tqdm
+
+import numpy as np
+import torch
 from torch.nn import MSELoss
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
-import torch
-import os
-from pathlib import Path
+from torch.utils.tensorboard import SummaryWriter
+
 import torchvision
 from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
-from tqdm import tqdm
 
-from src.dataset import dataset
+from src.customdataset import CustomDataset
 
 LR = 0.002
 
 
-class trainer():
-    def __init__(self, model, traindataset, testdataset, optimizer, loss_fn, name, cuda=0) -> None:
+class Trainer:
+    def __init__(self, model, traindataset: CustomDataset, testdataset: CustomDataset, optimizer, name: str, cuda: int = 0) -> None:
+        """
+        Trainer class to train model
+        :param model: model to train
+        :param traindataset: dataset to train on
+        :param testdataset: dataset to valid model while trainingsprocess
+        :param optimizer: optimizer to use
+        :param name: name of the model in savefiles and tensorboard
+        :param cuda: number of used cuda device
+        """
         self.device = torch.device(f"cuda:{cuda}") if torch.cuda.is_available() else torch.device('cpu')
         print(f"using {self.device}")
+
         self.model = model.to(self.device)
         self.trainloader = DataLoader(traindataset, batch_size=1, shuffle=True, num_workers=0)
         self.testloader = DataLoader(testdataset, batch_size=1, shuffle=False, num_workers=0)
         self.optimizer = optimizer(self.model.parameters(), lr=LR)
-        self.loss_fn = loss_fn
+
         self.bestavrgloss = None
+        self.step = 0
         self.name = name
+
+        # setup tensor board
+        train_log_dir = f'{Path(__file__).parent.absolute()}/../logs/runs/{self.name}'
+        print(f"{train_log_dir=}")
+        self.writer = SummaryWriter(train_log_dir)
 
     def save(self, name: str = ''):
         """
@@ -34,17 +54,25 @@ class trainer():
         os.makedirs(f'{Path(__file__).parent.absolute()}/../models/', exist_ok=True)
         torch.save(self.model.state_dict(), f'{Path(__file__).parent.absolute()}/../models/{name}')
 
-    def train(self, epoch):
-        for e in range(1, epoch+1):
+    def train(self, epoch: int):
+        """
+        train model for number of epochs
+        :param epoch: number of epochs
+        :return:
+        """
+        for e in range(1, epoch + 1):
             print(f"start epoch {e}:")
-            self.trainepoch()
-            avrgloss=self.valid()
-            if self.bestavrgloss==None or self.bestavrgloss>avrgloss:
-                self.bestavrgloss = avrgloss
+            self.train_epoch()
+            avgloss = self.valid()
+            if self.bestavrgloss is None or self.bestavrgloss > avgloss:
+                self.bestavrgloss = avgloss
                 self.save(f"{self.name}_es.pt")
 
-    def trainepoch(self):
-        avrgloss = []
+    def train_epoch(self):
+        """
+        train one epoch
+        :return:
+        """
         for img, target in tqdm(self.trainloader):
             img.to(self.device)
             # target.to(self.device)
@@ -60,32 +88,66 @@ class trainer():
             loss = sum(v for v in output.values())
             loss.backward()
             self.optimizer.step()
-            avrgloss.append(loss.cpu().detach())
-            print(loss.cpu().detach())
+
+            # logging
+            self.writer.add_scalar(f'Training/loss', loss.detach().cpu().item(), global_step=self.step)
+            self.writer.add_scalar(f'Training/loss_classifier', output['loss_classifier'].detach().cpu().item(),
+                                   global_step=self.step)
+            self.writer.add_scalar(f'Training/loss_box_reg', output['loss_box_reg'].detach().cpu().item(),
+                                   global_step=self.step)
+            self.writer.add_scalar(f'Training/loss_objectness', output['loss_objectness'].detach().cpu().item(),
+                                   global_step=self.step)
+            self.writer.add_scalar(f'Training/loss_rpn_box_reg', output['loss_rpn_box_reg'].detach().cpu().item(),
+                                   global_step=self.step)
+            self.writer.flush()
+            self.step += 1
+
             del img, target, output, loss
-        avgloss = torch.mean(avrgloss)
 
     def valid(self):
-        avrgloss = []
-        for img,target in self.testloader:
+        """
+        valid current model on validation set
+        :return: current loss
+        """
+        loss, loss_classifier, loss_box_reg, loss_objectness, loss_rpn_box_reg = [], [], [], [], []
+
+        for img, target in self.testloader:
             img.to(self.device)
             target.to(self.device)
-            pred = self.model(img)
-            loss = self.loss_fn(pred, target)
-            avrgloss.append(loss.cpu().detach())
-            del img, target, pred, loss
-        avgloss = torch.mean(avrgloss)
-        return avgloss
+            output = self.model(img, target)
+
+            loss.append(sum(v for v in output.values()).cpu().detach())
+            loss_classifier.append(output['loss_classifier'].detach().cpu().item())
+            loss_box_reg.append(output['loss_box_reg'].detach().cpu().item())
+            loss_objectness.append(output['loss_objectness'].detach().cpu().item())
+            loss_rpn_box_reg.append(output['loss_rpn_box_reg'].detach().cpu().item())
+
+            del img, target, output
+
+        meanloss = np.mean(loss)
+
+        # logging
+        self.writer.add_scalar(f'Valid/loss', meanloss, global_step=self.step)
+        self.writer.add_scalar(f'Valid/loss_classifier', np.mean(loss_classifier),
+                               global_step=self.step)
+        self.writer.add_scalar(f'Valid/loss_box_reg', np.mean(loss_box_reg),
+                               global_step=self.step)
+        self.writer.add_scalar(f'Valid/loss_objectness', np.mean(loss_objectness),
+                               global_step=self.step)
+        self.writer.add_scalar(f'Valid/loss_rpn_box_reg', np.mean(loss_rpn_box_reg),
+                               global_step=self.step)
+        self.writer.flush()
+
+        return meanloss
 
 
 if __name__ == '__main__':
-  model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
-  traindataset = dataset(f'{Path(__file__).parent.absolute()}/../data/GloSAT/preprocessed', 'tables')
-  validdataset = dataset(f'{Path(__file__).parent.absolute()}/../data/GloSAT/preprocessed', 'tables')
-  optimizer = AdamW
-  loss_fn = MSELoss()
-  name = 'firsttest'
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+    traindataset = CustomDataset(f'{Path(__file__).parent.absolute()}/../data/GloSAT/preprocessed', 'tables')
+    validdataset = CustomDataset(f'{Path(__file__).parent.absolute()}/../data/GloSAT/preprocessed', 'tables')
+    optimizer = AdamW
+    loss_fn = MSELoss()
+    name = 'test2'
 
-  trainer = trainer(model, traindataset, validdataset, optimizer, loss_fn, name)
-  trainer.train(1)
-
+    trainer = Trainer(model, traindataset, validdataset, optimizer, loss_fn, name)
+    trainer.train(1)
